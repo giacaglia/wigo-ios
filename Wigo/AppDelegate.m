69892e2bde817a8a9dc306f7af01e60331e92cda
Giuliano Giacaglia <raulzito234@gmail.com>
2014-07-09 11:38:23 -0400

    Facebook Login now prompts to the facebook app

--- Wigo/AppDelegate.m
+++ Wigo/AppDelegate.m
@@ -122,4 +122,19 @@ didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
     [firstTab setTitlePositionAdjustment:UIOffsetMake(0, -2)];
 }
 
+# pragma mark - Facebook Login
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    
+    // Call FBAppCall's handleOpenURL:sourceApplication to handle Facebook app responses
+    BOOL wasHandled = [FBAppCall handleOpenURL:url sourceApplication:sourceApplication];
+    
+    // You can add your app-specific url handling code here if needed
+    
+    return wasHandled;
+}
+
 @end
