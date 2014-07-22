08e4039535b9be04b77ba697a75e09548e4b5122
Giuliano Giacaglia <raulzito234@gmail.com>
2014-07-21 23:23:11 -0400

    Follow Request table view

--- Wigo/FollowRequestsViewController.m
+++ Wigo/FollowRequestsViewController.m
@@ -8,6 +8,7 @@
 
 #import "FollowRequestsViewController.h"
 #import "Globals.h"
+#import "SDWebImage/UIImageView+WebCache.h"
 
 @interface FollowRequestsViewController ()
 
@@ -49,11 +50,36 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Table View Data Source
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = (UITableViewCell*)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+//    User *user = (User *)[_everyoneParty getObjectWithId:[notifcation fromUserID]];
+    User *user = [[User alloc] init];
+    UIImageView *profileImageView = [[UIImageView alloc] init];
+    profileImageView.frame = CGRectMake(10, 10, 35, 35);
+    profileImageView.contentMode = UIViewContentModeScaleAspectFill;
+    profileImageView.clipsToBounds = YES;
+    [profileImageView setImageWithURL:[NSURL URLWithString:[user coverImageURL]]];
+    profileImageView.layer.cornerRadius = 5;
+    profileImageView.layer.borderWidth = 0.5;
+    profileImageView.layer.borderColor = [UIColor whiteColor].CGColor;
+    profileImageView.layer.masksToBounds = YES;
+    [cell.contentView addSubview:profileImageView];
+    
+    return cell;
+}
+
 #pragma mark - Network Function
 - (void)fetchFollowRequests {
     NSString *queryString = @"/api/notifications/?type=follow.request";
     [Network queryAsynchronousAPI:queryString withHandler:^(NSDictionary *jsonResponse, NSError *error) {
         dispatch_async(dispatch_get_main_queue(), ^(void){
+            [_followRequestTableView reloadData];
         });
     }];
 }
