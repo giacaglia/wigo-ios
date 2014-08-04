3d0ace9891ae9c8b7ed01e516e546561d1048e67
Giuliano Giacaglia <raulzito234@gmail.com>
2014-08-04 15:24:28 -0400

    Added boyvoice to the project.

---
+++ Wigo/LockScreenUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  LockScreenUIViewController.h
+//  Wigo
+//
+//  Created by Giuliano Giacaglia on 8/4/14.
+//  Copyright (c) 2014 Giuliano Giacaglia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LockScreenUIViewController : UIViewController
+
+@end
