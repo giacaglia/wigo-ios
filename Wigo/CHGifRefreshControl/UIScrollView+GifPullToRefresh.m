28034005446f57f1125ee7a28fbf01bb3055aba4
Giuliano Giacaglia <raulzito234@gmail.com>
2014-08-15 11:23:29 -0400

    Added transparent dancing G

--- Wigo/CHGifRefreshControl/UIScrollView+GifPullToRefresh.m
+++ Wigo/CHGifRefreshControl/UIScrollView+GifPullToRefresh.m
@@ -146,7 +146,8 @@ static char UIScrollViewGifPullToRefresh;
 {
     // HACK FOR IOS 8
     if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8")) {
-        if (self.scrollView.contentOffset.y == -64) [self.scrollView setContentOffset:CGPointZero animated:NO];
+        if (self.scrollView.contentOffset.y == -64)
+            [self.scrollView setContentOffset:CGPointZero animated:NO];
     }
     if (_state != GifPullToRefreshStateLoading) {
         if (self.scrollView.isDragging && self.scrollView.contentOffset.y + self.originalContentInsectY < -GifRefreshControlHeight && !_isTrigged) {
