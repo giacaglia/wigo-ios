1b83cbe3b25b7e49c352cca3e70df569a2d9417c
Giuliano Giacaglia <raulzito234@gmail.com>
2014-08-23 12:39:33 -0400

    Added hashtag

--- Wigo/LockScreenViewController.m
+++ Wigo/LockScreenViewController.m
@@ -159,7 +159,7 @@ OnboardFollowViewController *onboardFollowViewController;
     }
 }
 - (void)sharedPressed {
-    NSArray *activityItems = @[@"Who is going out tonight? http://wigo.us/app",[UIImage imageNamed:@"wigoApp" ]];
+    NSArray *activityItems = @[@"Who is going out tonight? #WiGo http://wigo.us/app",[UIImage imageNamed:@"wigoApp" ]];
     UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:nil];
     activityVC.excludedActivityTypes = @[UIActivityTypeCopyToPasteboard, UIActivityTypePrint, UIActivityTypeAssignToContact, UIActivityTypeAirDrop, UIActivityTypeSaveToCameraRoll];
     [self presentViewController:activityVC animated:YES completion:nil];
