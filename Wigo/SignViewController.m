195eb97e37c11ca6624ff8911a753c16a1d6610a
Giuliano Giacaglia <raulzito234@gmail.com>
2014-10-27 14:51:42 -0400

    Changing how the app responds to Facebook error messages.

--- Wigo/SignViewController.m
+++ Wigo/SignViewController.m
@@ -304,17 +304,55 @@
 }
 
 - (void) loginView:(FBLoginView *)loginView handleError:(NSError *)error {
-    if ([[[error userInfo] allKeys] containsObject:@"com.facebook.sdk:ErrorInnerErrorKey"]) {
-        NSError *innerError = [[error userInfo] objectForKey:@"com.facebook.sdk:ErrorInnerErrorKey"];
-        if ([[innerError domain] isEqualToString:NSURLErrorDomain]) {
-            [self showErrorNoConnection];
-        }
+    [self handleAuthError:error];
+//    if ([[[error userInfo] allKeys] containsObject:@"com.facebook.sdk:ErrorInnerErrorKey"]) {
+//        NSError *innerError = [[error userInfo] objectForKey:@"com.facebook.sdk:ErrorInnerErrorKey"];
+//        if ([[innerError domain] isEqualToString:NSURLErrorDomain]) {
+//            [self showErrorNoConnection];
+//        }
+//
+//    }
+//    else if ([[[error userInfo] allKeys] containsObject:@"NSLocalizedFailureReason"]) {
+//        if ([[[error userInfo] objectForKey:@"NSLocalizedFailureReason"] isEqualToString:@"com.facebook.sdk:SystemLoginDisallowedWithoutError"])
+//            [self showErrorLoginFailed];
+//    }
+}
 
+- (void)handleAuthError:(NSError *)error {
+    NSString *alertText;
+    NSString *alertTitle;
+    if ([FBErrorUtility shouldNotifyUserForError:error] == YES){
+        // Error requires people using you app to make an action outside your app to recover
+        alertTitle = @"Something went wrong";
+        alertText = [FBErrorUtility userMessageForError:error];
+        [self showMessage:alertText withTitle:alertTitle];
+        
+    } else {
+        // You need to find more information to handle the error within your app
+        if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryUserCancelled) {
+            //The user refused to log in into your app, either ignore or...
+            alertTitle = @"Login cancelled";
+            alertText = @"You need to login to access this part of the app";
+            [self showMessage:alertText withTitle:alertTitle];
+            
+        } else {
+            // All other errors that can happen need retries
+            // Show the user a generic error message
+            alertTitle = @"Something went wrong";
+            alertText = @"Please retry";
+            [self showMessage:alertText withTitle:alertTitle];
+        }
     }
-    else if ([[[error userInfo] allKeys] containsObject:@"NSLocalizedFailureReason"]) {
-        if ([[[error userInfo] objectForKey:@"NSLocalizedFailureReason"] isEqualToString:@"com.facebook.sdk:SystemLoginDisallowedWithoutError"])
-            [self showErrorLoginFailed];
-    }
+}
+
+- (void)showMessage:(NSString *)text withTitle:(NSString *)title
+{
+    [[[UIAlertView alloc] initWithTitle:title
+                                message:text
+                               delegate:self
+                      cancelButtonTitle:@"OK"
+                      otherButtonTitles:nil] show];
+    [self loginUserAsynchronous];
 }
 
 - (void) loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
