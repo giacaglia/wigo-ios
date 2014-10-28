0a9ac625e166355f228340ba63cfde39c93a7a50
Giuliano Giacaglia <raulzito234@gmail.com>
2014-10-28 17:31:39 -0400

    Battery in the main view controller is not in the main view controller

--- Wigo/MainViewController.m
+++ Wigo/MainViewController.m
@@ -20,7 +20,6 @@
 #import "CSStickyHeaderFlowLayout.h"
 #import "PeopleViewController.h"
 #import "NSObject-CancelableScheduledBlock.h"
-#import "BatteryViewController.h"
 
 @interface MainViewController ()
 
