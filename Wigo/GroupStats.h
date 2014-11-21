b2af293f3b185dc3655f5182e4d89721f5802602
Alex Grinman <grinman.alex@gmail.com>
2014-11-21 15:36:12 -0500

    initial ui for ambassador dashboard

---
+++ Wigo/GroupStats.h
@@ -0,0 +1,14 @@
+//
+//  GroupStats.h
+//  Wigo
+//
+//  Created by Alex Grinman on 11/21/14.
+//  Copyright (c) 2014 Alex Grinman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Globals.h"
+
+@interface GroupStats : UITableViewController
+
+@end
