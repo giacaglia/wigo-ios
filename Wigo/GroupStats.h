816c2cdaa6216daeceb5e5e6754d790d0b2c5579
Alex Grinman <grinman.alex@gmail.com>
2014-11-21 18:35:23 -0500

    graph and data now working! should be ready soon

--- Wigo/GroupStats.h
+++ Wigo/GroupStats.h
@@ -8,7 +8,20 @@
 
 #import <UIKit/UIKit.h>
 #import "Globals.h"
+#import "EngagementGraph.h"
 
-@interface GroupStats : UITableViewController
+@interface GroupStats : NSObject
+
+@property (nonatomic, strong) NSNumber *todayUserCount;
+@property (nonatomic, strong) NSNumber *weekUserCount;
+@property (nonatomic, strong) NSNumber *monthUserCount;
+@property (nonatomic, strong) NSNumber *allUsersCount;
+
+@property (nonatomic, strong) EngagementGraph *dailyEngagement;
+@property (nonatomic, strong) EngagementGraph *weeklyEngagement;
+@property (nonatomic, strong) EngagementGraph *monthlyEngagement;
+
++ (void) loadStats: (void (^)(GroupStats *groupStats, NSError *error)) handler;
 
 @end
+
