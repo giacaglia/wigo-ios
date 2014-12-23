43aadf696773cd758813787cb8050e49fb1a0060
Giuliano Giacaglia <raulzito234@gmail.com>
2014-12-23 18:18:11 -0200

    Fixed Edit profile > Photo > cancel button doesn't work

--- Wigo/EditProfileViewController.m
+++ Wigo/EditProfileViewController.m
@@ -167,7 +167,7 @@ UIViewController *webViewController;
     else {
         [self.view endEditing:YES];
         self.photoViewController = [[PhotoViewController alloc] initWithImage:[[[Profile user] images] objectAtIndex:buttonSender.tag]];
-        [[RWBlurPopover instance] presentViewController:self.photoViewController withOrigin:30 andHeight:([[UIScreen mainScreen] bounds].size.height - 50) fromViewController:self.navigationController];
+        [[RWBlurPopover instance] presentViewController:self.photoViewController withOrigin:0 andHeight:[[UIScreen mainScreen] bounds].size.height fromViewController:self.navigationController];
     }
 }
 
