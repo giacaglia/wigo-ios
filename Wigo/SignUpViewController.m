e2954506e1edfc784470cb8df2ae50d6c9015a8a
Giuliano Giacaglia <raulzito234@gmail.com>
2015-01-12 10:04:40 -0500

    Remove blankspaces of email when user is signing up.

--- Wigo/SignUpViewController.m
+++ Wigo/SignUpViewController.m
@@ -126,6 +126,7 @@
 
 - (void)continuePressed {
     NSString *emailString = _studentTextField.text;
+    emailString = [emailString stringByReplacingOccurrencesOfString:@" " withString:@""];
     NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
     BOOL isEmail = [emailTest evaluateWithObject:emailString];
