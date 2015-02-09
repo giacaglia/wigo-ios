eb617885209208ccc37c191aa0e20d02e6dbe4a9
Giuliano Giacaglia <raulzito234@gmail.com>
2015-02-09 15:35:19 -0500

    Surpress some warnings

--- Wigo/UIImageCategories/UIImage+Resize.m
+++ Wigo/UIImageCategories/UIImage+Resize.m
@@ -167,6 +167,8 @@
             transform = CGAffineTransformTranslate(transform, 0, newSize.height);
             transform = CGAffineTransformRotate(transform, -M_PI_2);
             break;
+        default:
+            break;
     }
     
     switch (self.imageOrientation) {
@@ -181,6 +183,7 @@
             transform = CGAffineTransformTranslate(transform, newSize.height, 0);
             transform = CGAffineTransformScale(transform, -1, 1);
             break;
+        default:break;
     }
     
     return transform;
