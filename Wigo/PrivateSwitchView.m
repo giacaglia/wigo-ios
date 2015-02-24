c3d73d55a8170742daa4e5c463717ab5f282f64d
Giuliano Giacaglia <raulzito234@gmail.com>
2015-02-24 13:35:21 -0500

    Update label for public events

--- Wigo/PrivateSwitchView.m
+++ Wigo/PrivateSwitchView.m
@@ -79,7 +79,7 @@
     
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(privacyPressed)];
     [self addGestureRecognizer:tapGesture];
-    self.explanationString = @"The whole school can see what you are posting.";
+    self.explanationString = @"The whole school can see and attend your event.";
     UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(move:)];
     [panRecognizer setMinimumNumberOfTouches:1];
     [panRecognizer setMaximumNumberOfTouches:1];
@@ -124,7 +124,7 @@
                  [[sender view] setFrame:CGRectMake(2, [sender view].frame.origin.y, [sender view].frame.size.width, [sender view].frame.size.height)];
             }];
             self.privacyTurnedOn = NO;
-            self.explanationString = @"The whole school can see what you are posting.";
+            self.explanationString = @"The whole school can see and attend your event.";
             [self.privateDelegate updateUnderliningText];
             self.publicLabel.hidden = NO;
             [self bringSubviewToFront:self.publicLabel];
