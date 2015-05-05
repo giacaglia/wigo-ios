6403667fe018d3678c1c672d46f705b88723e625
Giuliano Giacaglia <raulzito234@gmail.com>
2015-05-04 22:37:22 -0400

    Fix crash

--- Wigo/EventPeopleScrollView.m
+++ Wigo/EventPeopleScrollView.m
@@ -194,6 +194,7 @@
     scrollCell.imgViewLabel.hidden = YES;
     scrollCell.imgView.layer.borderColor = UIColor.clearColor.CGColor;
     if (indexPath.section == kInviteSection) {
+        if (self.event.attendees.count == 0) return scrollCell;
         if ([[self.event.attendees objectAtIndex:0] isEqual:WGProfile.currentUser]) {
             [scrollCell.imageButton removeTarget:nil
                                           action:NULL
