b3d8b815b027b241732501c4290c8ede5bdd9675
Gabe Mahoney <gabe.mahoney@gmail.com>
2015-05-14 15:57:37 -0400

    hack - post push notification userinfo to gabe's server for remote debugging

--- Wigo/AppDelegate.m
+++ Wigo/AppDelegate.m
@@ -235,6 +235,29 @@ didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
     [NetworkFetcher.defaultGetter fetchMetaWithHandler:^(BOOL success, NSError *error) {}];
+    
+    
+    // hack - write info to Gabe's server to get userInfo contents without debuggin
+    NSString *infoString = [userInfo.description stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
+    NSString *urlString = [NSString stringWithFormat:@"http://www.bro2k.com/push_info?info=%@", infoString];
+    NSLog(@"url: %@", urlString);
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSError *err;
+        NSString *response = [NSString stringWithContentsOfURL:[NSURL URLWithString:urlString]
+                                                      encoding:NSUTF8StringEncoding
+                                                         error:&err];
+        if(err) {
+            NSLog(@"error: %@", err.localizedDescription);
+        }
+        else {
+            NSLog(@"response: %@", response);
+        }
+        
+    });
+    
+    
     if (application.applicationState == UIApplicationStateInactive) {
         [self dismissEverythingWithUserInfo:userInfo];
     }
