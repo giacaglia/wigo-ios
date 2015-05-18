6d6472855a02310d8d9aa71fd98da727caa73045
Giuliano Giacaglia <raulzito234@gmail.com>
2015-05-18 14:53:34 -0400

    friends ids in the people screen

--- Wigo/PeopleViewController.m
+++ Wigo/PeopleViewController.m
@@ -79,6 +79,7 @@ NSIndexPath *userIndex;
     [super viewDidAppear:animated];
     self.navigationItem.titleView.tintColor = UIColor.whiteColor;
     self.title = self.user.firstName;
+    [NetworkFetcher.defaultGetter fetchFriendsIds];
     [self initializeTitleView];
     [self initializeRightBarButton];
 
