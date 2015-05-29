8a09b3a4ba306ffdcd7718c636590e435fcad8a9
Giuliano Giacaglia <raulzito234@gmail.com>
2015-05-28 23:05:30 -0400

    cleanup mess

--- Wigo/WGProfile.m
+++ Wigo/WGProfile.m
@@ -118,7 +118,6 @@ static BOOL isLocal = YES;
 }
 
 -(NSString *) key {
-    return @"0026cjAKWyXemocHRXM6TsdgEx";
     if ([self objectForKey:kKeyKey]) {
         return [self objectForKey:kKeyKey];
     }
