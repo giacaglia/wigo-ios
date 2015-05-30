5a7a7d52321986658ff2bfcdae192c6099637b56
Giuliano Giacaglia <raulzito234@gmail.com>
2015-05-30 14:50:40 -0400

    Finkelsten

--- Wigo/ProfileViewController.m
+++ Wigo/ProfileViewController.m
@@ -383,7 +383,7 @@ BOOL blockShown;
     _locationImgView.image = [UIImage imageNamed:@"locationIcon"];
     [_headerButtonView addSubview:_locationImgView];
     
-    _locationLabel = [[UILabel alloc] initWithFrame:CGRectMake(30, 9, 0.75*self.view.frame.size.width - 30, 14)];
+    _locationLabel = [[UILabel alloc] initWithFrame:CGRectMake(30, 6, 0.75*self.view.frame.size.width - 30, 20)];
     _locationLabel.text = self.user.group.name;
     _locationLabel.font = [FontProperties mediumFont:12.0f];
     _locationLabel.textAlignment = NSTextAlignmentLeft;
@@ -487,8 +487,8 @@ BOOL blockShown;
         numberOfProperties -= 1;
     }
     if (numberOfProperties == 1) {
-        CGAffineTransform scaleTrans  = CGAffineTransformMakeScale(2.2f, 2.2f);
-        CGAffineTransform leftToRightTrans  = CGAffineTransformMakeTranslation(5.0f, 2.2f);
+        CGAffineTransform scaleTrans  = CGAffineTransformMakeScale(2.0f, 2.1f);
+        CGAffineTransform leftToRightTrans  = CGAffineTransformMakeTranslation(5.0f, 2.0f);
         CGAffineTransform transl = CGAffineTransformConcat(scaleTrans, leftToRightTrans);
         _workLabel.textColor = RGB(170, 170, 170);
         _workLabel.font = [FontProperties mediumFont:18.0f];
@@ -500,7 +500,7 @@ BOOL blockShown;
         _schoolImgView.transform = transl;
         _locationImgView.transform = transl;
         
-        CGAffineTransform newTrans  = CGAffineTransformMakeTranslation(10.0f, 2.2f);
+        CGAffineTransform newTrans  = CGAffineTransformMakeTranslation(10.0f, 2.0f);
 
         _locationLabel.transform = newTrans;
         _locationLabel.center = _schoolLabel.center;
